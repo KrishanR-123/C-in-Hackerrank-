6215aa1524646b1225c1f4632c46dd8b88c8f2f0
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-29 20:07:09 +0530

    Hackerrank Solution

--- "Hello World!" in C.c
+++ "Hello World!" in C.c
@@ -6,8 +6,9 @@
 int main ()
 {
     char s[100];
-    fgets(s, sizeof(s), stdin);
-    printf("Hello, World!\n%s", s);
-    
-    return 0;
+scanf("%[^\n]%*c", s);
+printf("Hello, World!\n");
+printf("%s", s);
+
+return 0;
 }
